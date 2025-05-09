a483eb44e27470c0bfb2f57ef76ce3405cd18b9a
Waterbootdev <wassermann.sp@gmail.com>
2025-05-09 10:10:53 +0200

    added HTMLNode parameter validation children + unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,22 +1,27 @@
+from functools import reduce
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         
         if(value is None and children is None):
-            HTMLNode.raise_exception("value or children has to be not none")
+            HTMLNode._raise_exception("value or children has to be not none")
 
-        if children is not None and (type(children) is not list or len(children) == 0):
-            HTMLNode.raise_exception("children has to be a None or a list of at least one child")
+        if children is not None and HTMLNode._is_not_list_of_html_nodes(children) :
+            HTMLNode._raise_exception("children has to be None or a list of at least one child")
 
         
         self.value = value # string
         self.children = children # list of HTMLNode's
         self.tag = tag # string
-        self.props = {} if props is None else  props if type(props) is dict else HTMLNode.raise_exception("props has to be a dict or None") 
+        self.props = {} if props is None else  props if type(props) is dict else HTMLNode._raise_exception("props has to be a dict or None") 
         # dict key-value pairs representing attributes
 
-    
     @staticmethod
-    def raise_exception(message):
+    def _is_not_list_of_html_nodes(children):
+        #children is not none
+        return type(children) is not list or len(children) == 0 or not reduce(bool.__and__, map(lambda child: isinstance(child, HTMLNode) ,children), True)
+        
+    @staticmethod
+    def _raise_exception(message):
         raise ValueError(message)
 
     def to_html(self):
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,10 +1,29 @@
 import unittest
 
 from htmlnode import HTMLNode
+from textnode import TextNode, TextType
 
 from random import randint
 
 class TestHTMLNode(unittest.TestCase):
+    
+    def test_is_not_list_of_html_nodes_true(self):
+        b = True
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([]), b)
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes({}), b)
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([9]), b)
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes(["htmlnode"]), b)
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([TextNode("a", TextType.LINKS, url="https://www.google.com")]), b)
+        node = HTMLNode(value="") 
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([node, 8]), b)
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([node, "hs;fkjh", node]), b)
+      
+    def test_is_not_list_of_html_nodes_false(self):
+        b = False
+        node = HTMLNode(value="") 
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([node]), b)
+        self.assertEqual(HTMLNode._is_not_list_of_html_nodes([node, node, node]), b)
+    
     @staticmethod
     def props_node(props):
         return HTMLNode(value="",props=props)
@@ -27,8 +46,8 @@ class TestHTMLNode(unittest.TestCase):
 
         self.assertEqual(expected, len(TestHTMLNode.props_node(props).props_to_html()))
     
-
-
+    
+        
 if __name__ == "__main__":
     unittest.main()
     
