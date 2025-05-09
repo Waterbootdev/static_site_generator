e45ab451ad9e963fc9fb07331930d213928aa3ec
Waterbootdev <wassermann.sp@gmail.com>
2025-05-09 10:38:36 +0200

    HTMLNode parameter validation changed

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,23 +2,38 @@ from functools import reduce
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         
-        if(value is None and children is None):
+        HTMLNode._validate_parameters(tag, value, children, props)
+
+        self.value = value
+        self.children = children 
+        self.tag = tag 
+        self.props = {} if props is None else  props 
+
+    @staticmethod
+    def _validate_parameters(tag, value, children, props):
+        if value is None and children is None:
             HTMLNode._raise_exception("value or children has to be not none")
 
-        if children is not None and HTMLNode._is_not_list_of_html_nodes(children) :
+        if HTMLNode._is_not_none_and_not_type(children, list) and HTMLNode._is_not_list_of_html_nodes(children) :
             HTMLNode._raise_exception("children has to be None or a list of at least one child")
 
-        
-        self.value = value # string
-        self.children = children # list of HTMLNode's
-        self.tag = tag # string
-        self.props = {} if props is None else  props if type(props) is dict else HTMLNode._raise_exception("props has to be a dict or None") 
-        # dict key-value pairs representing attributes
+        if HTMLNode._is_not_none_and_not_type(props, dict):
+            HTMLNode._raise_exception("props has to be None or a dict")
+
+        if HTMLNode._is_not_none_and_not_type(value, str):
+            HTMLNode._raise_exception("value has to be None or a str")
+
+        if HTMLNode._is_not_none_and_not_type(tag, str):
+            HTMLNode._raise_exception("tag has to be None or a str")
+      
+    @staticmethod
+    def _is_not_none_and_not_type(parameter, parameter_type):
+        return parameter is not None and type(parameter) is not parameter_type 
 
     @staticmethod
     def _is_not_list_of_html_nodes(children):
-        #children is not none
-        return type(children) is not list or len(children) == 0 or not reduce(bool.__and__, map(lambda child: isinstance(child, HTMLNode) ,children), True)
+        #children is list
+        return len(children) == 0 or not reduce(bool.__and__, map(lambda child: isinstance(child, HTMLNode) ,children), True)
         
     @staticmethod
     def _raise_exception(message):
