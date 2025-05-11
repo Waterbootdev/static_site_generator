556a5a2f975f417321fd71862b188faa457b68da
Waterbootdev <wassermann.sp@gmail.com>
2025-05-12 00:06:56 +0200

    main for local and github

--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@ from io_helpers import copy_static_to_public, get_basepath
 from template_type import generate_pages_recursive
 import sys
 
-
+def main_public(public, basepath):
+    copy_static_to_public(public=public)
+    generate_pages_recursive("content", "template.html", public, basepath)
 
 def main():    
-    public = 'docs'
-    basepath = get_basepath(sys.argv)
-    print(f"basepath = {basepath}")
-    copy_static_to_public(public=public)
-    generate_pages_recursive("content", "template.html", public, f"/static_site_generator{basepath}")
-
+   
+    main_public('public', '/') #local
+    main_public('docs', '/static_site_generator/') #github
+   
 main()
 
     
