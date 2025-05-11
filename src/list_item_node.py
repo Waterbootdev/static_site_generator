2e1e65198133c94cf31a6d501ffb1019a29abc5e
Waterbootdev <wassermann.sp@gmail.com>
2025-05-11 20:05:03 +0200

    list_item_node refactored

--- src/list_item_node.py
+++ src/list_item_node.py
@@ -1,12 +1,8 @@
-from normal_text_node import NORMALTextNode
-from parent_node import ParentNode
+from none_list_parent_node import NoneListParentNode
 from commen_helpers import trim_left_first
 
-class ListItemNode(ParentNode):
+class ListItemNode(NoneListParentNode):
     def __init__(self, line):
+        super().__init__('li', trim_left_first(' ', line)[1])
 
-        super().__init__('li', NORMALTextNode.text_to_html_nodes(trim_left_first(' ', line)[1]))
-
-    @staticmethod
-    def list_item_nodes(block):
-        return list(map(ListItemNode, block.splitlines()))
+    
--- src/list_parent_node.py
+++ src/list_parent_node.py
@@ -2,5 +2,5 @@ from parent_node import ParentNode
 from list_item_node import ListItemNode
 
 class ListParantNode(ParentNode):
-    def __init__(self, tag, list_block, props=None):
-        super().__init__(tag, ListItemNode.list_item_nodes(list_block), props)
+    def __init__(self, tag, list_block, props=None):    
+        super().__init__(tag, list(map(ListItemNode, list_block.splitlines())), props)
